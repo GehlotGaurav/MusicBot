1fa860b9c770e5e2c3fdae6dc004d82b36fff0ed
Gaurav Gehlot <75985096+GehlotGaurav@users.noreply.github.com>
2024-08-09 21:32:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002214735864"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
